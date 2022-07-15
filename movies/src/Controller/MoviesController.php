11abc8706694653c99eb5e674e6f8fb31e16ed86
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-07-15 15:33:16 +0000

    get data with EntityManagerInterface

--- movies/src/Controller/MoviesController.php
+++ movies/src/Controller/MoviesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\MovieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,9 +11,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class MoviesController extends AbstractController
 {
     #[Route('/movies', name: 'movies')]
-    public function index(MovieRepository $movieRepository): Response
-    {
-        $movies = $movieRepository->findAll();
+    public function index(EntityManagerInterface $em): Response
+    {   
+        $repository = $em->getRepository(Movie::class);
+        $movies = $repository->findAll();
         return $this->render('index.html.twig',['movies' => $movies]);
     }
 }
