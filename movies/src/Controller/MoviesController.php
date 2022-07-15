45b9f93f0f3f1b539e91864160f020f3f533734a
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-07-15 12:14:46 +0000

    Old routes method

--- movies/src/Controller/MoviesController.php
+++ movies/src/Controller/MoviesController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
@@ -13,6 +14,19 @@ class MoviesController extends AbstractController
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/MoviesController.php',
+        ]);
+    }
+    
+    /**
+     * oldMethod
+     *
+     * @Route("/old", name="old")
+     * @return Response
+     */
+    public function oldMethod(): Response{
+        return $this->json([
+            'message' => 'Old Method',
             'path' => 'src/Controller/MoviesController.php',
         ]);
     }
