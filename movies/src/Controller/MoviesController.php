8ac46cae6c59392825f2118255b53d7b56f2748c
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-07-16 15:05:53 +0000

    get data from controller

--- movies/src/Controller/MoviesController.php
+++ movies/src/Controller/MoviesController.php
@@ -2,34 +2,25 @@
 
 namespace App\Controller;
 
-use App\Entity\Movie;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
-    private $em;
-    public function __construct(EntityManagerInterface $em){
-        $this->em = $em;
+    private $movieRepository;
+    public function __construct(MovieRepository $movieRepository){
+        $this->movieRepository = $movieRepository;
     }
 
-    #[Route('/movies', name: 'movies_show')]
+    #[Route('/movies', name: 'movies')]
     public function index(): Response
     {   
-        $repository = $this->em->getRepository(Movie::class);
-        $movies = $repository->findAll();
+        $movies = $this->movieRepository->findAll();
 
-        return $this->render('movies/index.html.twig',['movies' => $movies]);
-    }
-
-    #[Route('/movies/{id}', name: 'movies')]
-    public function show($id): Response
-    {   
-        $repository = $this->em->getRepository(Movie::class);
-        $movies = $repository->find($id);
-
-        return dd($movies);
+        return $this->render('movies/index.html.twig', [
+            'movies' => $movies
+        ]);
     }
 }
