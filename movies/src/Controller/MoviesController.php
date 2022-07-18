0fb158635f211158410d8b022d26a1ed75c8d840
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-07-18 11:43:01 +0000

    delete data

--- movies/src/Controller/MoviesController.php
+++ movies/src/Controller/MoviesController.php
@@ -118,14 +118,14 @@ class MoviesController extends AbstractController
         ]);
     }
 
-    #[Route('/movies/delete/{id}', name: 'delete_movie')]
+    #[Route('/movies/delete/{id}', methods:['GET', 'DELETE'], name: 'delete_movie')]
     public function delete($id): Response
     {   
         $movie = $this->movieRepository->find($id);
+        $this->em->remove($movie);
+        $this->em->flush();
 
-        return $this->render('movies/delete.html.twig', [
-            'movie' => $movie
-        ]);
+        return $this->redirectToRoute('movies');
     }
 
     #[Route('/movies/{id}', methods: ['GET'], name: 'show_movie')]
