77bf007987357cd025702502b77781e70ac87a7e
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-07-20 15:58:51 +0000

    error message

--- jwt/rest.php
+++ jwt/rest.php
@@ -1,7 +1,12 @@
 <?php
+require_once('constants.php');
+
     class Rest{
 
         public function __construct(){
+            if($_SERVER['REQUEST_METHOD'] !== 'POST'){
+                $this->throwError(REQUEST_METHOD_NOT_VALID, 'Invalid Request Method');
+            }
             $handler = fopen('php://input','r');
             $request = stream_get_contents($handler);
         }
@@ -19,7 +24,10 @@
         }
 
         public function throwError($code, $message){
-
+            header("content-type: application/json; charset=UTF-8");
+            $errorMsg = json_encode(["error" => ['status' => $code, 'message' => $message]]);
+            echo $errorMsg;
+            exit;
         }
 
         public function returnResponse(){
