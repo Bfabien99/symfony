a93c56b4f24b27c5f36c26db0d163b873fa2efdf
Bfabien99 <91257945+Bfabien99@users.noreply.github.com>
2022-07-20 14:34:16 +0000

    Rest class

--- jwt/rest.php
+++ jwt/rest.php
@@ -1,5 +1,29 @@
 <?php
     class Rest{
-        
+
+        public function __construct(){
+
+        }
+
+        public function validateRequest(){
+
+        }
+
+        public function processApi(){
+
+        }
+
+        public function validateParameters($fieldName, $value, $dataType, $required){
+
+        }
+
+        public function throwError($code, $message){
+
+        }
+
+        public function returnResponse(){
+
+        }
+
     }
 ?>
